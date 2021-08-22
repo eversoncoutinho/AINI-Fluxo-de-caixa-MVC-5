21d90167ebbef88f9a1e3bb7ec3ef5dbff8e75f4
eversoncoutinho <eversoncoutinho@gmail.com>
2021-08-22 09:15:55 -0300

    Filtro entrada e saída

--- AINI Fluxo de caixa/Models/Operation.cs
+++ AINI Fluxo de caixa/Models/Operation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace AINI_Fluxo_de_caixa.Models
     public class Operation
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Data  { get; set; }
+        [Display(Name = "Nome da Operação")]
         public string Nome { get; set; }
+        [Display(Name = "Tipo (E-entrada, S-Saida)")]
         public string Tipo { get; set; } //E -  entrada S - Saida
         public decimal Valor { get; set; }
     }
